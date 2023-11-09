11fd70aa4b58d63a200612f43efcaf003f4ecc3b
JoshTheConfused <97866996+JoshTheConfused@users.noreply.github.com>
2023-11-09 21:59:16 +0000

    All Work Done in Class

--- conditionals.js
+++ conditionals.js
@@ -1,7 +1,13 @@
 function quizGame() {
-    let answer1 = prompt("What is the capital of France?").toLowerCase();
+  let answer1 = prompt("What is the capital of France?").toLowerCase();
 
     // 1. Write a conditonal statement that checks if answer1 is equal to "paris".
+  if (answer1 === "paris") {
+    alert("Correct!");
+  }
+  else {
+    alert("Wrong!");
+  }
     //  - If true, alert "Correct!"
     //  - Else, alert "Wrong!"
     //  - Note: here's an example of what using an alert looks like: alert("Correct!")
@@ -9,9 +15,15 @@ function quizGame() {
 
     
    
-    let answer2 = parseInt(prompt("How many legs does an insect have? (Enter a number)"));
+  let answer2 = parseInt(prompt("How many legs does an insect have? (Enter a number)"));
 
     // 2. Write a conditonal statement that checks if answer2 is 6.
+  if (answer2 === 6) {
+    alert("Correct");
+  }
+  else {
+    alert("Too " + (answer2 < 6 ? "low" : "high") + "! Insects have 6 legs");
+  }
     //  - If true, alert "Correct!"
     //  - Else if answer2 is less than 6, alert "Too low! Insects have 6 legs."
     //  - Else, alert "Too high! Insects have 6 legs."
@@ -20,9 +32,9 @@ function quizGame() {
 
 
 
-    alert("Thanks for playing the quiz game!");
+  alert("Thanks for playing the quiz game!");
 }
 
 
 // 3. Once you've completed the conditionals, uncomment the line below and run your code.
-// quizGame();
+quizGame();
